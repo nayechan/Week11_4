8f1a4ac26d624726b3b9db2500ddd20c84da3e0a
yunsoo <dev.yunsoo@gmail.com>
2025-10-02 03:02:16 +0900

    fix: 여러 개에 빌보드를 붙여 넣고 텍스쳐를 변경한다면 , 동시에 변하는 이슈 해결

--- TL2/UI/Widget/TargetActorTransformWidget.cpp
+++ TL2/UI/Widget/TargetActorTransformWidget.cpp
@@ -294,7 +294,7 @@ AActor* UTargetActorTransformWidget::GetCurrentSelectedActor() const
 {
 	if (!UIManager)
 		return nullptr;
-		
+
 	return UIManager->GetSelectedActor();
 }
 
@@ -767,19 +767,16 @@ void UTargetActorTransformWidget::RenderWidget()
 			if (UBillboardComponent* Billboard = Cast<UBillboardComponent>(EditingCompBB))
 			{
 				ImGui::Text("Billboard Texture");
-			
+
 				int currentIdx = -1;
-				if (UMaterial* M = Billboard->GetMaterial())
+				const FString& cur = Billboard->GetTextureName();
+				for (int i = 0; i < 3; ++i)
 				{
-					const FString& cur = M->GetTextName();
-					for (int i = 0; i < 3; ++i)
+					const FString item = kDisplayItems[i];
+					if (cur.size() >= item.size() && cur.compare(cur.size() - item.size(), item.size(), item) == 0)
 					{
-						const FString item = kDisplayItems[i];
-						if (cur.size() >= item.size() && cur.compare(cur.size() - item.size(), item.size(), item) == 0)
-						{
-							currentIdx = i;
-							break;
-						}
+						currentIdx = i;
+						break;
 					}
 				}
 				ImGui::SetNextItemWidth(240);
@@ -787,7 +784,7 @@ void UTargetActorTransformWidget::RenderWidget()
 				{
 					if (currentIdx >= 0 && currentIdx < 3)
 					{
-						Billboard->SetTexture(kFullPaths[currentIdx]);
+						Billboard->SetTextureName(kFullPaths[currentIdx]);
 					}
 				}
 				ImGui::Separator();
