3053604f5ef1d0c65678a2412fb506f0d2e20712
fuenell <wlscjf1404@gmail.com>
2025-10-15 22:26:45 +0900

    feat: fog 컴포넌트 생성 UI 추가

--- TL2/FSceneRenderer.cpp
+++ TL2/FSceneRenderer.cpp
@@ -258,10 +258,10 @@ void FSceneRenderer::GatherVisibleProxies()
 					Proxies.FireBalls.Add(FireBallComponent);
 				}
 			}
-			//else if (UHeightFogComponent* FogComponent = Cast<UHeightFogComponent>(Component); FogComponent && bDrawFog)
-			//{
-			//	SceneGlobals.Fogs.Add(FogComponent);
-			//}
+			else if (UHeightFogComponent* FogComponent = Cast<UHeightFogComponent>(Component); FogComponent && bDrawFog)
+			{
+				SceneGlobals.Fogs.Add(FogComponent);
+			}
 		}
 	}
 }
@@ -394,7 +394,7 @@ void FSceneRenderer::RenderFireBallPass()
 	RHIDevice->OMSetDepthStencilState(EComparisonFunc::LessEqualReadOnly); // 깊이 쓰기 OFF
 	RHIDevice->OMSetBlendState(true); // 블렌딩 ON
 
-	for (UFireBallComponent* FireBall: Proxies.FireBalls)
+	for (UFireBallComponent* FireBall : Proxies.FireBalls)
 	{
 		// FireBall 렌더링
 		TArray<UPrimitiveComponent*> TargetPrimitives;
@@ -430,7 +430,7 @@ void FSceneRenderer::RenderPostProcessingPasses()
 	{
 		if (SceneGlobals.Fogs[0])
 		{
-			// SceneGlobals.Fogs[0] 를 사용
+			UHeightFogComponent* FogComponent = SceneGlobals.Fogs[0];
 		}
 	}
 
