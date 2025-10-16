f61ac66d75d34bec4c3c231b669c08db9a6f8d78
nansu0425 <nansu0425@gmail.com>
2025-10-16 21:45:16 +0900

    fix: 디버그 빌드의 ReportLiveDeviceObjects 제거

--- TL2/Source/Runtime/RHI/D3D11RHI.cpp
+++ TL2/Source/Runtime/RHI/D3D11RHI.cpp
@@ -1258,21 +1258,11 @@ void D3D11RHI::ReleaseDeviceAndSwapChain()
         DeviceContext = nullptr;
     }
 
-#if defined(_DEBUG)
-    ID3D11Debug* pDebug = nullptr;
-    if (SUCCEEDED(Device->QueryInterface(__uuidof(ID3D11Debug), (void**)&pDebug)))
-    {
-        pDebug->ReportLiveDeviceObjects(D3D11_RLDO_DETAIL);
-        pDebug->Release();
-    }
-#endif
-
     if (Device)
     {
         Device->Release();
         Device = nullptr;
     }
-
 }
 
 void D3D11RHI::OMSetDepthStencilState(EComparisonFunc Func)
