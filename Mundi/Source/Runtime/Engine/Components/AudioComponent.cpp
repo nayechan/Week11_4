f355c0157e493426157edba220232f9b9902b0e5
GameTith <wkdwobum@gmail.com>
2025-11-05 21:08:11 +0900

    feat: Sound 복구

---
+++ Mundi/Source/Runtime/Engine/Components/AudioComponent.cpp
@@ -0,0 +1,110 @@
+﻿// Audio component that plays USound via FAudioDevice
+
+#include "pch.h"
+#include "AudioComponent.h"
+#include "../GameFramework/FAudioDevice.h"
+#include "../Audio/Sound.h"
+#include "../../Core/Misc/PathUtils.h"
+#include <xaudio2.h>
+#include "ResourceManager.h"
+
+
+IMPLEMENT_CLASS(UAudioComponent)
+
+BEGIN_PROPERTIES(UAudioComponent)
+    MARK_AS_COMPONENT("오디오 컴포넌트", "소리를 듣고 싶습니꽈?!#!!")
+    ADD_PROPERTY_AUDIO(EPropertyType::Sound, Sound, "Sound", true)
+    ADD_PROPERTY(float, Volume, "Audio", true, "Volume (0..1)")
+    ADD_PROPERTY(float, Pitch,  "Audio", true, "Pitch (frequency ratio)")
+    ADD_PROPERTY(bool,  bIsLooping, "Audio", true, "Loop playback")
+    ADD_PROPERTY(bool,  bAutoPlay,  "Audio", true, "Auto play on BeginPlay")
+END_PROPERTIES()
+
+UAudioComponent::UAudioComponent()
+    : Volume(1.0f)
+    , Pitch(1.0f)
+    , bIsLooping(false)
+    , bAutoPlay(true)
+    , bIsPlaying(false)
+{
+}
+
+UAudioComponent::~UAudioComponent()
+{
+    Stop();
+}
+
+void UAudioComponent::BeginPlay()
+{
+    Super::BeginPlay();
+    if (bAutoPlay)
+    {
+        Play();
+    }
+}
+
+void UAudioComponent::TickComponent(float DeltaTime)
+{
+    Super::TickComponent(DeltaTime);
+
+    if (bIsPlaying && SourceVoice)
+    {
+        FVector CurrentLocation = GetWorldLocation();
+        FAudioDevice::UpdateSoundPosition(SourceVoice, CurrentLocation);
+
+        if (!bIsLooping)
+        {
+            XAUDIO2_VOICE_STATE state{};
+            SourceVoice->GetState(&state);
+            if (state.BuffersQueued == 0)
+            {
+                Stop();
+            }
+        }
+    }
+}
+
+void UAudioComponent::EndPlay()
+{
+    Super::EndPlay();
+    Stop();
+} 
+
+void UAudioComponent::Play()
+{
+    USound* Selected = Sound;
+   
+    if (!Selected)
+        return;
+
+    if (bIsPlaying)
+        return;
+
+    FVector CurrentLocation = GetWorldLocation();
+    SourceVoice = FAudioDevice::PlaySound3D(Selected, CurrentLocation, Volume, bIsLooping);
+
+    if (SourceVoice)
+    {
+        SourceVoice->SetFrequencyRatio(Pitch);
+        bIsPlaying = true;
+    }
+}
+
+void UAudioComponent::Stop()
+{
+    if (!bIsPlaying)
+        return;
+
+    if (SourceVoice)
+    {
+        FAudioDevice::StopSound(SourceVoice);
+        SourceVoice = nullptr;
+    }
+    bIsPlaying = false;
+}
+
+void UAudioComponent::DuplicateSubObjects()
+{
+    Super::DuplicateSubObjects();
+}
+
---
+++ Mundi/Source/Runtime/Engine/Components/AudioComponent.h
@@ -0,0 +1,68 @@
+﻿#pragma once
+#include "SceneComponent.h"
+
+
+// 실제 파일을 로드한 USound
+// 아직 USound가 없으므로 임시 객체
+class USound;
+struct IXAudio2SourceVoice;
+
+class UAudioComponent : public USceneComponent
+{
+public:
+	DECLARE_CLASS(UAudioComponent, USceneComponent)
+	GENERATED_REFLECTION_BODY();
+
+	UAudioComponent();
+	virtual ~UAudioComponent();
+
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime) override;
+	virtual void EndPlay() override;
+
+	/**
+	* @brief 이 컴포넌트에 할당된 Sound를 재생합니다.
+	*/
+	void Play();
+
+	/**
+	* @brief 현재 재생 중인 사운드를 정지합니다.
+	*/
+	void Stop();
+
+	/**
+	* @brief 재생할 사운드 리소스를 할당합니다.
+	*/
+	void SetSound(USound* NewSound) { Sound = NewSound; }
+
+public:
+
+	/** 재생할 사운드 리소스 */
+	USound* Sound;
+
+	/** 재생할 사운드 리소스의 볼륨 */
+	float Volume;
+
+	/** 재생할 사운드 리소스 높낮이 */
+	float Pitch;
+
+	/** 반복 재생 여부 */
+	bool bIsLooping;
+	/** 시작할 때 자동으로 재생시킬 지*/
+
+	/** UI에서 설정하는 사운드 파일 경로(.wav) */
+	bool bAutoPlay;
+
+	// Duplication
+	virtual void DuplicateSubObjects() override;
+	DECLARE_DUPLICATE(UAudioComponent)
+
+protected:
+	/** 현재 재생중인가*/
+	bool bIsPlaying;
+
+	/** 음원 재생기 like MP3 */
+	IXAudio2SourceVoice* SourceVoice = nullptr;
+};
+
+
