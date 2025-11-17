db6d649c2c446f36e10d777a97be73d618358d61
김민찬 <mcminchan1021@gmail.com>
2025-11-17 19:54:36 +0900

    fix: 스켈레탈 뷰어를 키면 그림자 맵 아틀라스 꽉 찼다는 로그가 안 뜨도록 수정

--- Mundi/Source/Runtime/Renderer/LightManager.cpp
+++ Mundi/Source/Runtime/Renderer/LightManager.cpp
@@ -553,7 +553,11 @@ void FLightManager::AllocateAtlasRegions2D(TArray<FShadowRenderRequest>& InOutRe
 		if (CurrentAtlasY + Request.Size > ShadowAtlasSize2D)
 		{
 			Request.Size = 0; // 꽉 참 (렌더링 실패)
-			UE_LOG("그림자 맵 아틀라스가 가득차서 더 이상 그림자를 추가할 수 없습니다.");
+			if (OwningWorld && !OwningWorld->IsPreviewWorld())
+			{
+				UE_LOG("그림자 맵 아틀라스가 가득차서 더 이상 그림자를 추가할 수 없습니다.");
+			}
+			
 			continue;
 		}
 
